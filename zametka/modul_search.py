c6fca820f8c570c0985c2f97c16ababd212a986b
Anton <m15.tony27@gmail.com>
2023-08-05 15:02:41 +0400

    Исправил ошибку в поиске заметки по ID.

--- zametka/modul_search.py
+++ zametka/modul_search.py
@@ -2,12 +2,15 @@ import json
 import view
 
 def search_id():
-    id = int(view.search_id())
-    with open("note_books.json", "r", encoding='utf-8') as read_file:
-        data = list(json.load(read_file))
-        for i in range(len(data)):
-            if data[i]["ID"]==id:
-                print(f'\nID : {str(data[i]["ID"])}'
-                      f'\nЗаголовок: {str(data[i]["Heading"])}'
-                      f'\nТело заметки: {str(data[i]["Body"])}'
-                      f'\nДата создания: {str(data[i]["Date/Time"])}')
+    try:
+        id = int(view.search_id())
+        with open("note_books.json", "r", encoding='utf-8') as read_file:
+            data = list(json.load(read_file))
+            for i in range(len(data)):
+                if data[i]["ID"]==id:
+                    print(f'\nID : {str(data[i]["ID"])}'
+                          f'\nЗаголовок: {str(data[i]["Heading"])}'
+                          f'\nТело заметки: {str(data[i]["Body"])}'
+                          f'\nДата создания: {str(data[i]["Date/Time"])}')
+    except:
+        print("\033[31m {}".format("\nДля начала создайте книгу заметок."),"\033[0m {}".format(" "))
